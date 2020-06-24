cf2c8eeef290fc8ec684605eb9fcba0307cad3f7
Gaetan <gaet97@live.fr>
2020-06-24 13:14:52 +0200

    Maj landing

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -12,6 +12,12 @@ import {ReactComponent as ThreeSteps} from '../../library/3steps.svg'
 import {ReactComponent as FirststepSneakers} from '../../library/firststepSneakers.svg'
 import {ReactComponent as FirststepRdv} from '../../library/firststepRdv.svg'
 
+import {ReactComponent as SecondStepExpert} from '../../library/secondStepExpert.svg'
+import {ReactComponent as SecondStepExpertIcon} from '../../library/secondStepExpertIcon.svg'
+
+import {ReactComponent as ThirdStepParcel} from '../../library/thirdStepParcel.svg'
+import {ReactComponent as ThirdStepParcelIcon} from '../../library/thirdStepParcelIcon.svg'
+
 const useStyles = makeStyles({
   brandName: {
     marginTop: 50,
@@ -35,8 +41,9 @@ const LandingContainer = () => {
         <Grid item><ThreeSteps/></Grid>
       </Grid>
 
-      <Grid container direction="row" justify="space-between">
-        <Grid item>
+      {/* FIRST STEP */}
+      <Grid container direction="row" style={{flexWrap: 'nowrap'}} justify="space-between">
+        <Grid item style={{marginLeft: 205}}>
           <Grid container direction="column">
             <Grid item>
               <FirststepSneakers/>
@@ -54,9 +61,58 @@ const LandingContainer = () => {
             </Grid>
           </Grid>
         </Grid>
-
         <Grid item>
           <FirststepRdv/>
+        </Grid>
+      </Grid>
+
+      {/* SECOND STEP */}
+      <Grid container direction="row" style={{flexWrap: 'nowrap'}} justify="space-between">
+        <Grid item>
+          <SecondStepExpert/>
+        </Grid>
+        <Grid item style={{marginRight: 205}}>
+          <Grid container direction="column">
+            <Grid item>
+              <SecondStepExpertIcon/>
+            </Grid>
+            <Grid item>
+              <Typography>
+                Un expert spécialisé pour ta sneakers
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+              Nous assurons le choix d’un expert spécialisé en fonction de la sneakers à authentifier. Une fois que tu nous as renseigné le modèle de la paire, nous contactons un expert qui colle parfaitement à celle-ci. 
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* THIRD STEP */}
+      <Grid container direction="row" style={{flexWrap: 'nowrap'}} justify="space-between">
+        <Grid item style={{marginLeft: 205}}>
+          <Grid container direction="column">
+            <Grid item>
+              <ThirdStepParcelIcon/>
+            </Grid>
+            <Grid item>
+              <Typography>
+                Une paire authentifiée et certifiée
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+              Une fois la paire authentifiée, tu recevras
+un message te signalant le resultat du legit check. Un certificat d’authenticité sera inséré dans ta boîte à chaussures ainsi qu’un jeton scellé à ta paire. 
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <ThirdStepParcel/>
         </Grid>
       </Grid>
 
